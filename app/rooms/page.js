c5b61f2aff54c7151d5a73d5841f427c7b3218be
Toma Milićević <tomatomamilicevic@gmail.com>
2025-10-15 12:19:15 +0200

    initial setup

---
+++ app/rooms/page.js
@@ -0,0 +1,11 @@
+import Counter from "../_components/Counter";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Rooms page</h1>
+
+      <Counter users={data} />
+    </div>
+  );
+}
