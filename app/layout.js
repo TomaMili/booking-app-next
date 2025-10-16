ec3c77c471e954bc1abf14d44b4aff1cd7c8084a
Toma Milićević <tomatomamilicevic@gmail.com>
2025-10-17 01:10:23 +0200

    Add metadata export to About, Account, and Rooms pages

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,31 @@
 import Navigation from "./_components/Navigation";
 import Logo from "./_components/Logo";
 
-import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "./_styles/globals.css";
+import Header from "./_components/Header";
+
+export const metadata = {
+  title: {
+    template: "%s / The Booking App",
+    default: "Welcome / The Booking App",
+  },
+  description: "Booking page for reserving hotel rooms in the forest hotel.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-blue-400 bg-">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
+        <Header />
+        <main className="text-4xl">{children}</main>
       </body>
     </html>
   );
--- app/rooms/page.js
+++ app/rooms/page.js
@@ -1,11 +1,15 @@
 import Counter from "../_components/Counter";
 
+export const metadata = {
+  title: "Rooms",
+};
+
 export default function Page() {
   return (
     <div>
       <h1>Rooms page</h1>
 
-      <Counter users={data} />
+      <Counter />
     </div>
   );
 }
