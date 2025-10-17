f453677750767b951e2c033ba8c5c14662401ff2
Toma Milićević <tomatomamilicevic@gmail.com>
2025-10-17 12:41:40 +0200

    Add image configuration to next.config.mjs for remote room images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ulhzgjehimepduybfjcw.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/room-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
