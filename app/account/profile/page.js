914eba30497cb774a0d51f59cedb2b03236cab95
Toma Milićević <tomatomamilicevic@gmail.com>
2025-10-20 01:44:46 +0200

    Update SideNavigation to highlight active link and modify profile page to use Image component for country flag

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,4 +1,5 @@
 import SelectCountry from "@/app/_components/SelectCountry";
+import Image from "next/image";
 
 export const metadata = {
   title: "Update profile",
@@ -6,7 +7,7 @@ export const metadata = {
 
 export default function Page() {
   // CHANGE
-  const countryFlag = "pt.jpg";
+  const countryFlag = "logo.png";
   const nationality = "portugal";
 
   return (
@@ -40,10 +41,12 @@ export default function Page() {
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <img
-              src={countryFlag}
+            <Image
+              src="/logo.png"
               alt="Country flag"
               className="h-5 rounded-sm"
+              width={10}
+              height={10}
             />
           </div>
 
