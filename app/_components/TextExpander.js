2a1303d33bd60c68ba99a195070f3f57928ad6f0
Toma Milićević <tomatomamilicevic@gmail.com>
2025-10-23 13:35:08 +0200

    Enhance TextExpander button styles for improved user interaction

--- app/_components/TextExpander.js
+++ app/_components/TextExpander.js
@@ -12,7 +12,7 @@ function TextExpander({ children }) {
     <span>
       {displayText}{" "}
       <button
-        className="text-primary-700 border-b border-primary-700 leading-3 pb-1"
+        className="text-primary-700 hover:text-primary-500 transition-all duration-150 border-b border-primary-700 leading-3 pb-1 cursor-pointer"
         onClick={() => setIsExpanded(!isExpanded)}
       >
         {isExpanded ? "Show less" : "Show more"}
